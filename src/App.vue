40dd337a0d495b578e4391630c7f5973dce7505d
ajs <140010746+aboodJS@users.noreply.github.com>
2026-08-13 17:03:50 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h1 class="syncopate-bold">Abdallah Alsawafeen</h1>
+  <h2 class="syncopate-bold">Frontend Developer</h2>
+</template>
+
+<style scoped></style>
