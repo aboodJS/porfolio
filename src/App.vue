6e7d0f8756f16a2998d44ac050ee6961cbe94179
ajs <140010746+aboodJS@users.noreply.github.com>
2026-08-14 13:30:36 +0300

    restarting the project

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup></script>
-
-<template>
-  <h1 class="syncopate-bold">Abdallah Alsawafeen</h1>
-  <h2 class="syncopate-bold">Frontend Developer</h2>
-</template>
-
-<style scoped></style>
